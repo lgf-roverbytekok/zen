9129f996c56308a4fea6fbf8e81721816a3897d5
Zen Software <ZenSoftware@users.noreply.github.com>
2024-02-08 18:02:29 -0700

    refactor(unity): removed dead space
    undefined

--- apps/zen-unity/Assets/Scripts/MultiplayerNetwork.cs
+++ apps/zen-unity/Assets/Scripts/MultiplayerNetwork.cs
@@ -8,16 +8,8 @@ namespace Zen
         async void Start()
         {
             ColyseusRoom<Colyseus.MyRoomState> room = await NetworkManager.ColyseusClient.JoinOrCreate<Colyseus.MyRoomState>("MainRoom");
-
-            room.OnJoin += () =>
-            {
-                Debug.Log("Joined MainRoom");
-            };
-
-            room.OnMessage<Colyseus.Player>("boxUpdate", message =>
-            {
-                Debug.Log("boxUpdate called");
-            });
+            room.OnJoin += () => Debug.Log("Joined MainRoom");
+            room.OnMessage<Colyseus.Player>("boxUpdate", message => Debug.Log("boxUpdate called"));
         }
     }
 }
